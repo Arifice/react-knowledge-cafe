112c61ded1d12a38b1769b2ddf7cdf33ff049cfb
Arifice <arifice13@gmail.com>
2023-09-14 00:13:00 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <h1 className='text-6xl p-4 bg-purple-600 font-bold text-white text-center'>Knowledge Cafe</h1>
+     
+    </>
+  )
+}
+
+export default App
